60a6455339f5c6b0610240e9d161541e9eab2c71
kami-sama <kamisamakirishima@gmail.com>
2026-03-26 11:55:37 +0530

    seeding data errors fixed

--- state.py
+++ state.py
@@ -1,8 +1,9 @@
 import os
+from datetime import datetime
 
 from dotenv import load_dotenv
 
-from orders import Orders, Order
+from orders import Coordinates, Order, Orders, OrderStatus
 
 load_dotenv()
 port = int(os.getenv("PORT", 5000))
@@ -12,9 +13,31 @@ orders = Orders()
 
 # For testing purposes
 # TEMPORARY ORDERS. In production, will added via POST req to /orders endpoint.
-orders.items = [
-    Order("001", "Shaurya Singh", 1.2, "918368458315", "A-167 SEC-122", 201301, Coordinates(1.2, 4.5),
-          datetime(2026, 3, 26, 3, 30)),
-    Order("002", "Sarthak", 1.5, "919871740518", "DELHI ME KAHI", 110067, Coordinates(1.5, 2.3),
-          datetime(2026, 3, 26, 3, 30))
-]
+orders.items.extend(
+    [
+        Order(
+            "001",
+            "Shaurya Singh",
+            1.2,
+            "918368458315",
+            "A-167 SEC-122",
+            201301,
+            Coordinates(1.2, 4.5),
+            300,
+            OrderStatus.PENDING,
+            datetime(2026, 3, 26, 3, 30),
+        ),
+        Order(
+            "002",
+            "Sarthak",
+            1.5,
+            "919871740518",
+            "DELHI ME KAHI",
+            110067,
+            Coordinates(1.5, 2.3),
+            300,
+            OrderStatus.PENDING,
+            datetime(2026, 3, 26, 3, 30),
+        ),
+    ]
+)
