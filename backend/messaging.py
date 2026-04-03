48f8a26ffbf6b1f71789a4c41aad192f59c17bbe
kami-sama <kamisamakirishima@gmail.com>
2026-04-04 00:04:32 +0530

    changed the time window

--- backend/messaging.py
+++ backend/messaging.py
@@ -1,7 +1,9 @@
-from state import orders
-from orders import OrderStatus
 from datetime import datetime, timedelta
+
+from orders import OrderStatus
 from routes import api
+from state import orders
+
 # datetime(2026, 3, 25, 3, 30)
 
 
@@ -11,7 +13,7 @@ def notify_orders():
         try:
             if order.status != OrderStatus.PENDING or order.notification_status:
                 continue
-            message_body = f"Hi {order.customer_name} 👋\n\nYour delivery is scheduled between:\n⏰ {(order.arrival - timedelta(minutes=83)).strftime('%H:%M')} - {(order.arrival + timedelta(minutes=37)).strftime('%H:%M')}\nPlease reply with:\n1️⃣ Confirm \n2️⃣ Reschedule\n\nOrder ID: {order.order_id}"
+            message_body = f"Hi {order.customer_name} 👋\n\nYour delivery is scheduled between:\n⏰ {order.get_time_window()}\nPlease reply with:\n1️⃣ Confirm \n2️⃣ Reschedule\n\nOrder ID: {order.order_id}"
 
         except Exception as e:
             # Step 7: Handle errors
